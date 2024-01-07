0449be97f18728cfd3de5594dbc1bda1d2df05e1
Dmitrii Kovanikov <kovanikov@gmail.com>
2024-01-07 14:59:21 +0000

    Implement CLI using Cmdliner

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,10 +1,16 @@
-open Core
+open Cmdliner
 
-let repo_param =
-  let open Command.Param in
-  anon ("owner/repo" %: string)
+let repo_arg =
+  let doc = "The GitHub repository to launch." in
+  Arg.(value & pos 0 string "NOT_SPECIFIED" & info [] ~docv:"OWNER/REPO" ~doc)
 
-let command =
-  Command.basic
-    ~summary:"Display GitHub TUI for the selected repository"
-    (Command.Param.map repo_param ~f:(fun _repo () -> Tui.start ()))
+let gh_tui_t = Term.(const Tui.start $ repo_arg)
+
+let cmd =
+  let doc = "TUI of a GitHub repository" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Submit bug reports at: https://github.com/chshersh/github-tui/issues" ]
+  in
+  let info = Cmd.info "gh-tui" ~version:"0.1.0" ~doc ~man in
+  Cmd.v info gh_tui_t
--- lib/tui.ml
+++ lib/tui.ml
@@ -1,2 +1,3 @@
 let app = Minttea.app ~init:Init.init ~update:Update.update ~view:View.view ()
-let start () = Minttea.start app ~initial_model:Model.initial_model
+
+let start _repo_name = Minttea.start app ~initial_model:Model.initial_model
