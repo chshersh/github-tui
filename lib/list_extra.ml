ab6ea0527a9776a5da769bd30dcc0507f89877b5
Dmitrii Kovanikov <kovanikov@gmail.com>
2024-02-11 13:16:50 +0000

    Print next files with file connector

--- lib/list_extra.ml
+++ lib/list_extra.ml
@@ -6,3 +6,10 @@ let in_between ~sep list =
   match list with
   | [] | [_] -> list
   | x :: xs -> x :: loop xs
+
+let generate n f =
+  let rec loop i =
+    if i = n
+      then []
+      else f i :: loop (i + 1)
+  in loop 0
--- lib/tui/view.ml
+++ lib/tui/view.ml
@@ -41,7 +41,7 @@ let tabs_section cur_tab =
         ];
     ]
 
-let current_level_to_doc (cursor: Fs.cursor) =
+let current_level_to_doc (cursor: Fs.cursor) has_next =
   let open Pretty in
 
   let files = Array.map Fs.file_name cursor.files in
@@ -60,6 +60,9 @@ let current_level_to_doc (cursor: Fs.cursor) =
   let bot = "╰" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "╯" in
 
   (* Line *)
+  let fmt_selected_line line =
+    "│ " ^ String_extra.fill_right max_name_len line ^ " ├"
+  in
   let fmt_line line =
     "│ " ^ String_extra.fill_right max_name_len line ^ " │"
   in
@@ -68,7 +71,11 @@ let current_level_to_doc (cursor: Fs.cursor) =
   (* Combine *)
   files
   |> Array.to_list
-  |> List.map fmt_line
+  |> List.mapi (fun i line ->
+    if i = cursor.pos && has_next
+      then fmt_selected_line line
+      else fmt_line line
+  )
   |> List_extra.in_between ~sep:mid
   |> (fun lines -> [top] @ lines @ [bot])
   |> List.mapi (fun i s ->
@@ -78,9 +85,10 @@ let current_level_to_doc (cursor: Fs.cursor) =
   )
   |> col
 
-let children_to_doc ~prev_total:_ ~pos:_ children =
+let children_to_doc ~prev_total ~pos children =
   let open Pretty in
 
+  (* This array is guaranteed to be non-empty at this point *)
   let files = Array.map Fs.file_name children in
   let max_name_len =
     files
@@ -92,23 +100,63 @@ let children_to_doc ~prev_total:_ ~pos:_ children =
   let max_len = max_name_len + 4 in
 
   (* Frame *)
-  let top = "╭" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "╮" in
-  let mid = "├" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "┤" in
-  let bot = "╰" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "╯" in
+  let top = "  ╭" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "╮" in
+  let mid = "│ ├" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "┤" in
+  let bot = "  ╰" ^ String_extra.repeat_txt (max_len - 2) "─" ^ "╯" in
 
-  (* Line *)
-  let fmt_line line =
-    "│ " ^ String_extra.fill_right max_name_len line ^ " │"
+  (* Connector arrow *)
+  let prev_rows_count = 2 * prev_total + 1 in
+  let connect_pos = 2 * pos + 1 in
+  let connector_doc =
+    List_extra.generate
+      prev_rows_count
+      (fun i ->
+        let is_current_pos = i = connect_pos in
+        if is_current_pos
+          then str "─"
+          else str " "
+      )
+    |> col
   in
 
-  (* Combine *)
-  files
-  |> Array.to_list
-  |> List.map fmt_line
-  |> List_extra.in_between ~sep:mid
-  |> (fun lines -> [top] @ lines @ [bot])
-  |> List.map str
-  |> col
+  (* Line *)
+  let fmt_line i line =
+    let is_first_pos = i = 0 in
+    let is_last_pos = i = Array.length children - 1 in
+    let has_more_than_one = Array.length children > 1 in
+    let prefix =
+      if is_first_pos then
+        (if has_more_than_one
+          then "┬"
+          else "─")
+      else if is_last_pos then
+        "└"
+      else
+        "├"
+    in
+    prefix ^ "─┤ " ^ String_extra.fill_right max_name_len line ^ " │"
+  in
+
+  (* Next level files *)
+  let files_doc =
+    files
+    |> Array.to_list
+    |> List.mapi fmt_line
+    |> List_extra.in_between ~sep:mid
+    |> (fun lines -> [top] @ lines @ [bot])
+    |> (fun lines ->
+      let pad_before =
+        List_extra.generate
+          (max (connect_pos - 1) 0)
+          (fun _ -> "")
+      in
+      pad_before @ lines
+    )
+    |> List.map str
+    |> col
+  in
+
+  row [connector_doc; files_doc]
 
 let next_level_to_doc ~prev_total ~pos (selected_file: Fs.tree) =
   (* Get the next level files *)
@@ -122,12 +170,12 @@ let next_level_to_doc ~prev_total ~pos (selected_file: Fs.tree) =
 
 let fs_doc (fs : Fs.zipper) =
   let current = fs.current in
-  let current_level_doc = current_level_to_doc current in
   let next_level_doc =
     Option.bind
       (Fs.file_at current)
       (next_level_to_doc ~prev_total:(Array.length current.files) ~pos:current.pos)
   in
+  let current_level_doc = current_level_to_doc current (Option.is_some next_level_doc) in
   match next_level_doc with
   | None ->
     current_level_doc
